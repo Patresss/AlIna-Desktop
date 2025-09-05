af1509a6bc556e7fc63f0f0dbf5a024c1e646d83
patrykpiechaczek <piechaczek.patryk@gmail.com>
2025-09-05 21:10:06 +0200

    Update build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,12 +41,6 @@ dependencies {
 
     implementation("io.github.mkpaz:atlantafx-base:2.1.0")
 
-    implementation("org.openjfx:javafx-controls:26-ea+2")
-    implementation("org.openjfx:javafx-fxml:26-ea+2")
-    implementation("org.openjfx:javafx-swing:26-ea+2")
-    implementation("org.openjfx:javafx-media:26-ea+2")
-    implementation("org.openjfx:javafx-web:26-ea+2")
-
     implementation("org.kordamp.ikonli:ikonli-javafx:12.3.1")
     implementation("org.kordamp.ikonli:ikonli-feather-pack:12.3.1")
     implementation("org.kordamp.ikonli:ikonli-material2-pack:12.3.1")
@@ -54,26 +48,11 @@ dependencies {
     implementation("org.kordamp.ikonli:ikonli-bootstrapicons-pack:12.3.1")
 
     implementation("org.jetbrains:annotations:23.0.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    implementation("org.assertj:assertj-core:3.21.0")
-    implementation("log4j:log4j:1.2.17")
-    implementation("org.slf4j:slf4j-api:1.7.5")
-    implementation("org.slf4j:slf4j-log4j12:1.7.5")
     implementation("com.github.kwhat:jnativehook:2.2.2")
     implementation("commons-io:commons-io:2.14.0")
     implementation("org.apache.commons:commons-lang3:3.14.0")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.2")
-
-
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-
     // common module
-    implementation("commons-io:commons-io:2.14.0")
-    implementation("log4j:log4j:1.2.17")
-    implementation("org.slf4j:slf4j-api:1.7.5")
-    implementation("org.slf4j:slf4j-log4j12:1.7.5")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.2")
 
@@ -86,12 +65,6 @@ dependencies {
 
 }
 
-configurations {
-    all {
-        exclude(group = "ch.qos.logback", module = "logback-classic")
-//        exclude(group = "org.slf4j", module = "slf4j-log4j12")
-    }
-}
 
 
 tasks.withType<Test> {
@@ -108,20 +81,3 @@ tasks.withType<JavaExec> {
     jvmArgs = runArgsValue
 }
 
-
-
-//val createInstaller = tasks.create("createInstaller") {
-//    doFirst {
-//        println("Create Installer")
-//    }
-//}
-
-//tasks.named("jpackage") {
-//    dependsOn(createInstaller)
-//}
-
-val packageExecutableZip = tasks.create<Zip>("packageExecutableZip") {
-    archiveFileName.set("$title-${project.version}.zip")
-    destinationDirectory.set(file("${rootDir}/release/${project.version}"))
-    from("$buildDir/image")
-}
